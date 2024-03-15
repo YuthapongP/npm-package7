f794da31c1f80554113cd0613f8b98874cd0fc0d
Nicky <yuthapong@magicboxsolution.com>
2024-03-15 13:07:37 +0700

    test 13

--- index.jsx
+++ index.jsx
@@ -1,41 +1,27 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-// import { ComponentProps } from "react";
 
 const StyledButton = styled.button`
-  ${({ color }) => css`
-    background-color: ${color || "#ff00e6"};
-    color: white;
-    font-size: 16px;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.2s;
+  background-color: #007bff;
+  color: white;
+  font-size: 16px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s;
 
-    &:hover {
-      background-color: "#0056b3";
-    }
-  `}
+  &:hover {
+    background-color: #0056b3;
+  }
 `;
 
-// export type ButtonComponentProps = ComponentProps<"button">;
-
-// type NormalReactComponentType = {
-//   children?: ReactNode;
-//   label?: string;
-//   color?: string;
-// };
-
-// type ComnbineType = ButtonComponentProps & NormalReactComponentType
-
-const NormalReactComponent = ({ children, color }) => {
+export const NormalReactComponent = () => {
   return (
     <div>
-      <StyledButton color={color}>{children}</StyledButton>
+      <StyledButton>Leave me alone, Don't click!</StyledButton>
     </div>
   );
 };
 
-export { NormalReactComponent };
