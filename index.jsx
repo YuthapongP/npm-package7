b849b0d6b4148699ba9ab0b4d0329a9316f0d736
Nicky <yuthapong@magicboxsolution.com>
2024-03-15 13:29:01 +0700

    test package 7

---
+++ index.jsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: #007bff;
+  color: white;
+  font-size: 16px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+function NormalButton() {
+  return (
+    <div>
+      <StyledButton>Click Me</StyledButton>
+    </div>
+  );
+}
+
+export default NormalButton;
